ce022110837af115aa6f45ded67ab0ce710359be
YoussefAtefHenin <youssef.atef.fci@gmail.com>
2026-08-15 01:56:45 +0300

    Add grocery app

---
+++ lib/grocery_app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_app/screens/login_screen.dart';
+import 'package:grocery_app/screens/register_screen.dart';
+
+class GroceryApp extends StatelessWidget {
+  const GroceryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      initialRoute: '/login',
+
+      routes: {
+        '/login': (context) => LoginScreen(),
+        '/register': (context) => LogoutScreen(),
+      },
+    );
+  }
+}
